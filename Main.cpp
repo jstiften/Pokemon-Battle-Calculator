14ec7aea4f3ca66514226f8d6f9e187beba8340e
jstiften <jstiften@gmail.com>
2021-06-01 15:39:18 -0500

    Added weather parsing

--- Main.cpp
+++ Main.cpp
@@ -9,29 +9,53 @@
 #include<stdlib.h>
 #include<iostream>
 #include"Pokemon.h"
+#include<string>
+#include<algorithm>
 
 using namespace std;
 
+void initGen1();
+string parseWeather(string userInput);
+
+Pokemon* charmander;
+Pokemon* charmeleon;
+Pokemon* charizard;
+
 int main(){
 
-	Pokemon Charizard;
-	Charizard.name = "Charizard";
-	Charizard.hp = 360;
-	Charizard.attack = 293;
-	Charizard.defense = 280;
-	Charizard.spAttack = 348;
-	Charizard.spDefense = 295;
-	Charizard.speed = 328;
+	initGen1();
+	cout << charmander->name << endl;
+	cout << charmeleon->name << endl;
+	cout << charizard->name << endl;
 
-	cout << Charizard.name << endl;
-	cout << "Charizard HP:" << Charizard.hp << endl;
-	cout << "Charizard Attack:" << Charizard.attack << endl;
-	cout << "Charizard Defense:" << Charizard.defense << endl;
-	cout << "Charizard Special Attack:" << Charizard.spAttack << endl;
-	cout << "Charizard Special Defense:" << Charizard.spDefense << endl;
-	cout << "Charizard Speed:" << Charizard.speed << endl;
+	string userWeather;
+	getline(cin, userWeather);
+	parseWeather(userWeather);
+	cout << userWeather << endl;
 
 	return 0;
 }
 
+void initGen1(){
+	charmander = createPokemon("Charmander", 282, 223, 203, 240, 201, 218);
+	charmeleon = createPokemon("Charmeleon", 320, 249, 236, 284, 251, 284);
+	charizard = createPokemon("Charizard", 360, 293, 280, 348, 295, 328);
+}
+
+
+string parseWeather(string userInput){
+	string weather;
+
+	if(userInput.compare("sun")){
+		weather = "sun";
+	}else if(userInput.compare("sand")){
+		weather = "sand";
+	}else if(userInput.compare("rain")){
+		weather = "rain";
+	}else if(userInput.compare("hail")){
+		weather = "hail";
+	}
+
+	return weather;
+}
 
--- Pokemon.h
+++ Pokemon.h
@@ -8,8 +8,12 @@
 #ifndef POKEMON_H_
 #define POKEMON_H_
 
+#include<string>
+
+using namespace std;
+
 struct Pokemon {
-	char* name;
+	string name;
 	int hp;
 	int attack;
 	int defense;
@@ -17,6 +21,20 @@ struct Pokemon {
 	int spDefense;
 	int speed;
 };
+
+struct Pokemon* createPokemon(string name, int hp, int attack, int defense, int spAttack, int spDefense, int speed){
+	struct Pokemon* pokemon = (struct Pokemon*)malloc(sizeof(struct Pokemon));
+
+	pokemon->name = name;
+	pokemon->hp = hp;
+	pokemon->attack = attack;
+	pokemon->defense = defense;
+	pokemon->spAttack = spAttack;
+	pokemon->spDefense = spDefense;
+	pokemon->speed = speed;
+
+	return pokemon;
+}
 
 
 
