509a7846c0c8ff257f0c9c344fb94f255cc9dd3f
jstiften <jstiften@gmail.com>
2021-06-01 14:35:45 -0500

    Init Commit

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Jun 1, 2021
+ *      Author: jakestiften
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+
+	return 0;
+}
+
+
---
+++ Pokemon.h
@@ -0,0 +1,17 @@
+/*
+ * Pokemon.h
+ *
+ *  Created on: Jun 1, 2021
+ *      Author: jakestiften
+ */
+
+#ifndef POKEMON_H_
+#define POKEMON_H_
+
+struct Pokemon {
+
+};
+
+
+
+#endif /* POKEMON_H_ */
