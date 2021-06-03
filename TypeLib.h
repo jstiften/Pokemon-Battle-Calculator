50ef934191c2e83e46b3c23a5f32aafcc4434d14
jstiften <jstiften@gmail.com>
2021-06-02 20:29:42 -0500

    Forgot type lib file

---
+++ TypeLib.h
@@ -0,0 +1,23 @@
+/*
+ * TypeLib.h
+ *
+ *  Created on: Jun 2, 2021
+ *      Author: jakestiften
+ */
+
+#ifndef TYPELIB_H_
+#define TYPELIB_H_
+
+#include"PokemonTypes.h"
+
+using namespace std;
+
+PokemonType* fire;
+PokemonType* water;
+PokemonType* grass;
+PokemonType* poison;
+PokemonType* flying;
+PokemonType* noSecondType;
+
+
+#endif /* TYPELIB_H_ */
